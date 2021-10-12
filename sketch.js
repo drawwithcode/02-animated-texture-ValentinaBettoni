2ee97a19822e2c25382369c46f7ac0973f60c663
ValentinaBettoni <wally.98@live.com>
2021-10-12 17:08:48 +0200

    tolta libreria suono

--- sketch.js
+++ sketch.js
@@ -1,13 +1,42 @@
-function preload() {
-  // put preload code here
-}
+var points = [];
+var mult = 0.03;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // put setup code here
+  background(0);
+  angleMode(DEGREES);
+  noiseDetail(1);
+
+  var density = 200;
+  var space = width / density;
+
+  for (var x = 0; x < width; x += space) {
+    for (var y = 0; y < height; y += space) {
+      var p = createVector(x + random(-10, 10), y + random(-10, 10));
+      points.push(p);
+    }
+  }
 }
 
 function draw() {
-  // put drawing code here
-  background("red");
+  noStroke();
+
+  for (var i = 0; i < points.length; i++) {
+    var r = map(points[i].x, 0, width, 50, 255);
+    var g = map(points[i].y, 0, height, 50, 255);
+    var b = map(points[i].x, 0, width, 255, 50);
+
+    fill(r, g, b);
+
+    var angle = map(
+      noise(points[i].x * mult, points[i].y * mult),
+      0,
+      1,
+      0,
+      720
+    );
+
+    points[i].add(createVector(cos(angle), sin(angle)));
+    ellipse(points[i].x, points[i].y, 1);
+  }
 }
